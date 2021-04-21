ddf72b3818166d0fcaf8f877a972f815b0333e99
Christiano Tavares <cltavares@gmail.com>
2021-04-21 19:02:37 -0300

    game cocos fase 1

---
+++ assets/Script/Btn_iniciar.js
@@ -0,0 +1,16 @@
+cc.Class({
+    extends: cc.Component,
+   
+    // use this for initialization
+    onLoad: function () {
+        cc.director.preloadScene('Reflorestamento');
+        
+        this.node.on('touchend', function(){
+            cc.log("TESTE");
+            window.score = 0;
+            cc.director.loadScene('Reflorestamento');
+            cc.director.resume();
+        });
+
+    },
+});
