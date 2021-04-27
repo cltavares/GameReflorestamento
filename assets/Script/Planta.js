060978ad1bf464eecae04a23682491051449b4c4
Christiano Tavares <cltavares@gmail.com>
2021-04-26 22:21:20 -0300

    ajustado o bug do pause pegava as plantas

--- assets/Script/Planta.js
+++ assets/Script/Planta.js
@@ -5,28 +5,17 @@ cc.Class({
     onLoad: function() {
   
         this.node.on('touchend', function(){
-            this.node.dispatchEvent(new cc.Event.EventCustom('planta_pega', true));
-            this.node.removeFromParent();
-            this.adicionarScore();
+
+            if(!cc.director.isPaused()){
+                this.node.dispatchEvent(new cc.Event.EventCustom('planta_pega', true));
+                this.node.removeFromParent();
+                this.adicionarScore();
+            }
+
         }, this);
     },
 
     adicionarScore: function() {
         score++;
     },
-
-    /*
-    onPause: function(){
-        if (cc.director.isPaused()){
-            cc.director.resume();
-            this.pauseLabel.node.active = false;
-        }
-        else{
-            cc.director.pause();
-            this.pauseLabel.node.active = true;
-        }
-        
-    },
-    */
-
 });
